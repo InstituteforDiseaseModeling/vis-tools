edf260978acf55bff61ad16c501b537aa2e5f7e2
Jen Schripsema <jen.schripsema@gatesfoundation.org>
2023-07-06 17:50:44 -0600

    added myst_parser and readthedocs-sphinx-search

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,9 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
     'sphinx.ext.todo',
-    'plantweb.directive'
+    'plantweb.directive',
+    'myst_parser', # source files can be written in Markdown or RST
+    'sphinx_search.extension' # search across all IDM docs
 ]
 
 plantuml = 'plantweb'
@@ -244,8 +246,23 @@ html_show_sphinx = False
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
 # base URL from which the finished HTML is served.
-#
-html_use_opensearch = ''
+
+# -- RTD Sphinx search for searching across the entire domain -------------
+
+if os.environ.get('READTHEDOCS') == 'True':
+
+    search_project_parent = "institute-for-disease-modeling-idm"
+    search_project = os.environ["READTHEDOCS_PROJECT"]
+    search_version = os.environ["READTHEDOCS_VERSION"]
+
+    rtd_sphinx_search_default_filter = f"subprojects:{search_project_parent}/{search_version}"
+
+    rtd_sphinx_search_filters = {
+        "Search this project": f"project:{search_project}/{search_version}",
+        "Search all IDM docs": f"subprojects:{search_project_parent}/{search_version}",
+    }
+
+#html_use_opensearch = ''
 
 # This is the file name suffix for HTML files (e.g. ".xhtml").
 # html_file_suffix = None
