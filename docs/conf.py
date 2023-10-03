3aa2a289470faa9e0b422684345e643772cc1db1
Jen Schripsema <jen.schripsema@gatesfoundation.org>
2023-10-03 17:16:49 -0600

    updated Sphinx docs with myst_parser extension, https://github.com/InstituteforDiseaseModeling/idm-content/issues/30

--- docs/conf.py
+++ docs/conf.py
@@ -45,6 +45,24 @@ extensions = [
     'plantweb.directive',
     'sphinx_search.extension', # search across multiple docsets in domain
     'sphinx.ext.viewcode', # link to view source code
+    'myst_parser', # source files written in MD or RST
+]
+
+myst_enable_extensions = [
+    "amsmath",
+    "attrs_inline",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+    "linkify",
+    "replacements",
+    "smartquotes",
+    "strikethrough",
+    "substitution",
+    "tasklist",
 ]
 
 plantuml = 'plantweb'
